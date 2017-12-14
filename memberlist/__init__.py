136807411a5a3f6b3aa75c0dc2112a6fb49a46e3
haliphax <haliphax@gmail.com>
2017-12-14 13:51:09 -0600

    block access to user profiles, too

--- memberlist/__init__.py
+++ memberlist/__init__.py
@@ -4,6 +4,12 @@
 from flask import current_app, request
 from flask_login import current_user
 
+endpoints = (
+    'forum.memberlist',
+    'forum.search',
+    'forum.user',
+)
+
 
 def flaskbb_request_processors(app):
     'Apply the login restriction.'
@@ -12,6 +18,6 @@ def flaskbb_request_processors(app):
     def before_request():
         'Check authentication before request is handled.'
 
-        if (request.endpoint in ('forum.memberlist', 'forum.search')
+        if (request.endpoint in endpoints
                 and not current_user.is_authenticated):
             return current_app.login_manager.unauthorized()
